18fbbd31511646747c7e192ae9d0bdec48440116
noemi15 <niighiis@hotmail.com>
2023-08-29 16:40:36 -0300

    tp2

---
+++ practicoNro2/Readme.md
@@ -0,0 +1,31 @@
+# Piedra papel o tijera 
+
+Juego para ocio
+
+## Como juego 🚀
+
+Para empezar es necesario ingresar tu  nombre, despues dale click al boton empezar .
+El juego te dará la bienvenida y te pedirá elegir piedra papel o tijera, jugarás con la computadora y en pantalla se mostrará las elecciones de ambos y el marcador por cada jugada, tenes 5 juegos a realizar pero si antes de ellos alguno lograra ganar 3 jugadas, se anunciará al ganador del juego y te dará la opcion de volver a jugar
+
+### Pre-requisitos 📋
+
+El juego es online, podes jugar sin problema.
+
+## Ejecutando las pruebas ⚙️
+
+Al ejecutar las pruebas se verificaron que al tener la posibilidad de varios empates en las jugadas, no se lograba estabablecer al ganador, asi que se agrego un resultador a tener esa opcion.
+
+### Y las pruebas de estilo de codificación ⌨️
+
+Para establecer mayor confort al usuario se generaron animaciones y tanto activacion y ocultar bloques para darle mayor dinamismo.
+
+## Construido con 🛠️
+
+Se pudo desarrollar este juego con los siguientes tutoriales:
+
+* https://www.w3schools.com/js/default.asp
+* https://www.youtube.com/watch?v=QZ6iJG1mQFM&t=1650s
+* https://www.youtube.com/watch?v=GMYbQVuQ4Cc&t=6s
+* https://www.youtube.com/watch?v=WBkcdoGFFjY 
+
+
---
+++ practicoNro2/ppt.js
@@ -0,0 +1,140 @@
+const btnPiedra = document.getElementById("piedra")
+const btnPapel = document.getElementById("papel")
+const btnTijera = document.getElementById("tijera")
+const nombreJugador = document.getElementById("nombre")
+const msjJugada = document.getElementById("msj-jugada")
+const msjJugadaFinal = document.getElementById("msj-jugada-final")
+const eleccionJugador = document.getElementById("eleccion-jugador")
+const eleccionPC = document.getElementById("eleccion-pc")
+const contenedorPuntosUsuario = document.querySelector("#puntos-usuario");
+const contenedorPuntosPC = document.querySelector("#puntos-computadora");
+const opcionesJuego = document.querySelector("#opciones");
+const marcador = document.getElementById("marcadorId");
+const inicio = document.getElementById("inicioId");
+const presentacion = document.getElementById("presentacionId");
+const reload = document.getElementById("reiniciar");
+
+let opcionJugador;
+let opcionPc;
+let cantJuegosGanadosJugador = 0;
+let cantJuegosGanadosPc = 0;
+let conteoJugada = 0;
+
+// ingreso datos del jugador , ocultar bloque
+ingresoNombre.addEventListener("click",() => {       
+    if(nombreJugador.value.length == 0){
+        alert("Campo vacio, favor de ingresar nombre");
+    } else {
+        document.getElementById("cargaNombre").innerHTML = nombreJugador.value;
+        jugador.innerHTML = nombreJugador.value;
+        inicio.style.display = "block";
+        presentacion.style.display = "none";
+       
+    }
+})
+
+// eleccion jugador
+btnPiedra.addEventListener('click', function(){
+        opcionJugador = "Piedra";
+        opPc();
+    });
+    
+btnPapel.addEventListener('click', function(){
+        opcionJugador = "Papel";
+        opPc();
+    });
+
+btnTijera.addEventListener('click', function(){
+        opcionJugador = "Tijera";
+        opPc();
+    })
+
+// eleccion de PC y conteo jugada
+function opPc(){
+        let aleaorio = nAleatorio();
+    
+        if(aleaorio == 0){
+            opcionPc = "Piedra";
+        }else if(aleaorio == 1){
+            opcionPc = "Papel";
+        }else if(aleaorio == 2){
+            opcionPc = "Tijera";
+        }  
+        
+        if(conteoJugada < 6){
+            jugada();
+            eleccionJugador.innerHTML = opcionJugador;
+            eleccionPC.innerHTML = opcionPc;
+            combate();
+            jugadaFinal();
+        }
+    }
+    
+
+// agrega imagen en funcion a las elecciones de la jugada
+function combate() {
+    if(opcionJugador == "Papel" && opcionPc == "Piedra" || opcionPc == "Papel" && opcionJugador == "Piedra"){
+        document.getElementById("combate").innerHTML =  '<img src="papel envuelve piedra.png">';
+    }else if(opcionJugador == "Piedra" && opcionPc == "Tijera" ||opcionPc == "Piedra" && opcionJugador == "Tijera" ){
+        document.getElementById("combate").innerHTML =  '<img src="piedra golpea tijera.png">';
+    }else if(opcionJugador == "Tijera" && opcionPc == "Papel" ||  opcionPc == "Tijera" && opcionJugador == "Papel"){
+        document.getElementById("combate").innerHTML =  '<img src="tijera corta papel.png">';
+    } else{
+        document.getElementById("combate").innerHTML =  '<img src="empate.png">';
+    }
+}
+
+// eleccion aleatoria
+function nAleatorio(){
+        let n = Math.floor(Math.random() * 3);
+        return n;
+        }
+
+// determinar ganador de una jugada e ingresar en pantalla
+function jugada() {
+    if (opcionJugador == "Piedra" && opcionPc == "Tijera" ||
+        opcionJugador == "Papel" && opcionPc == "Piedra" ||
+        opcionJugador == "Tijera" && opcionPc == "Papel"){
+            msjJugada.innerHTML = "Ganó Jugador!";                
+            cantJuegosGanadosJugador++;
+            conteoJugada++;
+            contenedorPuntosUsuario.innerText = cantJuegosGanadosJugador;
+    }else if (opcionPc == "Piedra" && opcionJugador == "Tijera" ||
+                opcionPc == "Papel" && opcionJugador == "Piedra" ||
+                opcionPc == "Tijera" && opcionJugador == "Papel"){
+            msjJugada.innerHTML = "Ganó PC!";
+            cantJuegosGanadosPc++;
+            conteoJugada++;
+            contenedorPuntosPC.innerText = cantJuegosGanadosPc;
+    }else{
+            msjJugada.innerHTML = "Empate"; 
+            conteoJugada++;
+            cantJuegosGanadosJugador++;
+            cantJuegosGanadosPc++;
+            contenedorPuntosUsuario.innerText = cantJuegosGanadosJugador;
+            contenedorPuntosPC.innerText = cantJuegosGanadosPc;
+    }         
+    marcador.style.display = "block";
+}
+
+
+// determinar al ganador de 3 juegos
+function jugadaFinal(){
+    if (cantJuegosGanadosJugador == 3 || cantJuegosGanadosPc == 3) {
+        if (cantJuegosGanadosJugador == 3) {
+            msjJugadaFinal.innerHTML = "🔥 ¡GANASTE EL JUEGO! 🔥";
+        } else if (cantJuegosGanadosPc == 3) {
+            msjJugadaFinal.innerHTML = "😭 ¡GANÓ LA PC!";
+        }/* else {
+            msjJugadaFinal.innerHTML = "😭 ¡GANARON, REVANCHA?!";
+        }*/
+        inicio.style.display = "none";   
+        reload.style.display = "block";
+    } 
+ } 
+
+// generar reinicio
+reload.addEventListener("click", _ => {
+    location.reload();
+});
+
